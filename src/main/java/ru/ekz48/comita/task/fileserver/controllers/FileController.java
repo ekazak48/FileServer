4026121eb5a5776a2d89832735cc01e9a7114fc2
Казаков Евгений <ekazak48@gmail.com>
2023-06-07 23:57:08 +0300

    added file uploading functionality (still need to add exception handling)

--- src/main/java/ru/ekz48/comita/task/fileserver/controllers/FileController.java
+++ src/main/java/ru/ekz48/comita/task/fileserver/controllers/FileController.java
@@ -1,11 +1,17 @@
 package ru.ekz48.comita.task.fileserver.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import ru.ekz48.comita.task.fileserver.services.FileStorageService;
 
 import java.util.List;
 
